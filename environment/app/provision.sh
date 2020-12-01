dae8491ef2b5a915d9bf4e72561c02e916f6f638
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-12-01 10:41:41 +0000

    removed node modules and added line in provision file to see it

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -28,4 +28,6 @@ sudo systemctl restart nginx
 # we cd into the app directory and start it
 # we use & to start it in the background
 cd /home/ubuntu/app
+rm -r node_modules
+npm install
 npm start &
